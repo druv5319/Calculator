082213f217648ab0cc70b4114a861d2729bf370b
Dhruv Patel <druv5319@gmail.com>
2018-06-13 13:15:36 -0700

    Optimized clickClear functionality.

--- app/src/main/java/com/example/android/calculator/MainActivity.java
+++ app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -4,6 +4,7 @@ import java.text.DecimalFormat;
 import java.text.DecimalFormat.*;
 import java.text.NumberFormat;
 import java.text.NumberFormat.*;
+import java.util.ArrayList;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     double result, firstvalue, secondvalue, percentvalue;
+    ArrayList<String> equationArrayList = new ArrayList<String>();
+    String currentDisplayNumber = "";
     String statement = "", firststatement = "", secondstatement, command, strresult;
-    int maxlength = 9, wholeresult, count;
+    int maxlength = 9, wholeresult, count, clearcount = 0;
     boolean equalsclickedlast = false, decimalpresent = false, negativepresent = false;
 
 
@@ -38,138 +41,131 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void numberButtonClicked(View numberView){
-        clearResult();
+        if (equalsclickedlast == true) {
+            equationArrayList.clear();
+            currentDisplayNumber = "";
+            displayResult(currentDisplayNumber);
+        }
+        equalsclickedlast = false;
         Button numberButton = (Button)numberView;
         String numberButtonString = numberButton.getText().toString();
         String tempNumber = "";
         if(numberButtonString.equalsIgnoreCase("+/-")) {
-            if (statement.substring(0,1).equalsIgnoreCase("-")) {
-                tempNumber = statement.substring(1, statement.length());
+            if (currentDisplayNumber.substring(0,1).equalsIgnoreCase("-")) {
+                tempNumber = currentDisplayNumber.substring(1, currentDisplayNumber.length());
             } else {
-                tempNumber = "-" + statement;
+                tempNumber = "-" + currentDisplayNumber;
             }
         } else {
-            tempNumber = statement + numberButtonString;
+            tempNumber = currentDisplayNumber + numberButtonString;
         }
         String numberPattern = "[-+]?[0-9]+[.]?[0-9]*";
         if (tempNumber.matches(numberPattern)){
-            statement=tempNumber;
-            displayResult(statement);
+            currentDisplayNumber=tempNumber;
+            displayResult(currentDisplayNumber);
+            Button clearButton = (Button)findViewById(R.id.clear);
+            if (currentDisplayNumber.length() > 0) {
+                clearButton.setText("C");
+            }
         }
     }
 
-    public void clickPercent(View c){
+    /*public void clickPercent(View c){
         if (statement != ""){
             percentvalue = Double.parseDouble(statement);
             percentvalue= percentvalue / 100;
             statement = Double.toString(percentvalue);
             displayResult(statement);
         }
-    }
+    }*/
+
 
     public void clickClear(View v) {
-        statement = "";
-        result = 0;
-        command = "";
-        displayResult("0");
+        Button buttonClear = (Button)findViewById(R.id.clear);
+        buttonClear.setText("AC");
+
+         TextView displayNumber = (TextView) findViewById(R.id.viewresult);
+        currentDisplayNumber = "";
+        displayNumber.setText("0");
+        if (clearcount==1 && equationArrayList != null){
+            equationArrayList.clear();
+            clearcount = 0;
+            buttonClear.setText("AC");
+        }
+        clearcount++;
+
+
+
     }
+
+
+
+
 // The if else statements in all of the function methods are used if the user presses the wrong function button or
 //changes their mind and decides to use a different function. Without these if else statements, it is not possible to change the
 //function without clearing (clear button) everything and restarting again
 
-    public void clickDivide(View c) {
-        if (statement != "") {
-            if (firststatement != "")
-                clickEquals(c);
-            firststatement = statement;
-            statement = "";
-            command = "divide";
-        }
-        else
-            command = "divide";
-    }
-
-    public void clickMultiply(View c) {
-        if (statement != "") {
-            if (firststatement != "")
-                clickEquals(c);
-            firststatement = statement;
-            statement = "";
-            command = "multiply";
-        }
-        else
-            command = "multiply";
-    }
-
-    public void clickSubtract(View c) {
-        if (statement != "") {
-            if (firststatement != "")
-                clickEquals(c);
-            firststatement = statement;
-            statement = "";
-            command = "subtract";
-
-        }
-        else
-            command = "subtract";
-    }
-
-    public void clickAdd(View c) {
-        if (statement != "") {
-            if (firststatement != "")
-                clickEquals(c);
-            firststatement = statement;
-            statement = "";
-            command = "add";
-        }
-        else
-            command = "add";
+    public void operatorClicked (View operatorView) {
+        Button operatorButton = (Button)operatorView;
+        String operatorButtonString = operatorButton.getText().toString();
+        equationArrayList.add(currentDisplayNumber);
+        currentDisplayNumber = "";
+        equationArrayList.add(operatorButtonString);
     }
 
     public void clickEquals(View c) {
-        if (firststatement != "" & statement != "") {
-            secondstatement = statement;
-            firstvalue = Double.parseDouble(firststatement);
-            secondvalue = Double.parseDouble(secondstatement);
-
-            //Tells the program which function to compute according to which function button is pressed
-            switch (command) {
-                case "divide":
-                    result = firstvalue / secondvalue;
-                    break;
-                case "multiply":
-                    result = firstvalue * secondvalue;
-                    break;
-                case "subtract":
-                    result = firstvalue - secondvalue;
-                    break;
-                case "add":
-                    result = firstvalue + secondvalue;
-                    break;
-
+        equationArrayList.add(currentDisplayNumber);
+        //TODO: Evaluate Equation
+        for (int i = 0; i < equationArrayList.size(); i++) {
+            if (equationArrayList.get(i).equalsIgnoreCase("×")) {
+                evaluateExpressionInEquationArrayList(i);
+                i-=2;
             }
-
-            //If the answer is a  whole number, the calcultor will display an Int instead of a double so the decimal values will
-            // not be shown
-            if (result % 1 == 0) {
-                wholeresult = (int) result;
-                strresult = Integer.toString(wholeresult);
-            } else {
-                // If the answer is a non-whole number, the calculator will display the answer with the decimal values
-                strresult = Double.toString(result);
-            }
-
-            if (strresult.length() >9) {
-                //If the result is too long to fit the app, this will shorten it into scientific notation
-                strresult = String.format("%.3E", result);
-            }
-            command = "";
-            decimalpresent = false;
-            statement = strresult;
-            firststatement = "";
-            equalsclickedlast = true;
-            displayResult(strresult);
         }
+
+        for (int i = 0; i < equationArrayList.size(); i++) {
+            if (equationArrayList.get(i).equalsIgnoreCase("÷")) {
+                evaluateExpressionInEquationArrayList(i);
+                i-=2;
+            }
+        }
+
+        for (int i = 0; i < equationArrayList.size(); i++) {
+            if (equationArrayList.get(i).equalsIgnoreCase("+")) {
+                evaluateExpressionInEquationArrayList(i);
+                i-=2;
+            }
+        }
+
+        for (int i = 0; i < equationArrayList.size(); i++) {
+            if (equationArrayList.get(i).equalsIgnoreCase("-")) {
+                evaluateExpressionInEquationArrayList(i);
+                i-=2;
+            }
+        }
+        currentDisplayNumber = equationArrayList.get(0);
+        equationArrayList.remove(0);
+        equalsclickedlast = true;
+        displayResult(currentDisplayNumber);
+    }
+
+    private void evaluateExpressionInEquationArrayList (int index) {
+        Double leftNumber = Double.parseDouble(equationArrayList.get(index-1));
+        Double rightNumber = Double.parseDouble(equationArrayList.get(index+1));
+        String operator = equationArrayList.get(index);
+        Double result;
+        if (operator.equalsIgnoreCase("×")) {
+            result = leftNumber*rightNumber;
+        } else if (operator.equalsIgnoreCase("÷")) {
+            result = leftNumber/rightNumber;
+        } else if (operator.equalsIgnoreCase("+")) {
+            result = leftNumber+rightNumber;
+        } else {
+            result = leftNumber-rightNumber;
+        }
+        equationArrayList.set(index-1, result.toString());
+        equationArrayList.remove(equationArrayList.get(index+1));
+        equationArrayList.remove(equationArrayList.get(index));
     }
 
     // This method to clear the current result/view on the screen when a number button is pushed
